01c3576e818c702898db640baaa6cc91cc05eb78
Deepak Verma <deepakv3010238@gmail.com>
2025-03-26 16:46:15 +0530

    Last commit by Deepak

--- src/components/habitList.tsx
+++ src/components/habitList.tsx
@@ -29,34 +29,35 @@ const HabitList = () => {
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
       {habits.map((habit) => (
         <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
-          <Grid conatiner alignItems="center">
-            <Grid textAlign="start" xs={12} sm={6}>
-              <Typography variant="h6">{habit.name}</Typography>
-              <Typography variant="body2" color="text.secondary">
-                {habit.frequency}
-              </Typography>
-            </Grid>
-            <Grid textAlign="start" xs={12} sm={6}>
-              <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
-                <Button
-                  variant="outlined"
-                  color={
-                    habit.completedDates.includes(today) ? "success" : "primary"
-                  }
-                  startIcon = {<CheckCircleIcon/>}
-                  onClick={() => toggleHabit(habit.id, today)}
-                >
-                 
-                  {habit.completedDates.includes(today)
-                    ? "Completed"
-                    : "Mark Complete"}
-                </Button>
-                <Button variant="outlined" color="error" startIcon = {<DeleteIcon/>} onClick={() => removeHabit(habit.id)}>
-                  Remove
-                </Button>
-              </Box>
-            </Grid>
-          </Grid>
+          <Grid container alignItems="center" justifyContent="space-between" spacing={2}>
+  <Grid item xs={12} sm={6} sx={{ textAlign: "start" }}>
+    <Typography variant="h6">{habit.name}</Typography>
+    <Typography variant="body2" color="text.secondary">
+      {habit.frequency}
+    </Typography>
+  </Grid>
+  <Grid item xs={12} sm={6}>
+    <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+      <Button
+        variant="outlined"
+        color={habit.completedDates.includes(today) ? "success" : "primary"}
+        startIcon={<CheckCircleIcon />}
+        onClick={() => toggleHabit(habit.id, today)}
+      >
+        {habit.completedDates.includes(today) ? "Completed" : "Mark Complete"}
+      </Button>
+      <Button
+        variant="outlined"
+        color="error"
+        startIcon={<DeleteIcon />}
+        onClick={() => removeHabit(habit.id)}
+      >
+        Remove
+      </Button>
+    </Box>
+  </Grid>
+</Grid>
+
           <Box sx={{mt:2}}>
             <Typography>Current Streak : {getStreak(habit)}</Typography>
             <LinearProgress variant="determinate" value={(getStreak(habit)/30) * 100} sx={{mt:1}}/>
